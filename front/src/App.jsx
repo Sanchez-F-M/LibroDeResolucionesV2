6ef925a6a32f38096d2af3bde90e64ac437f78a8
FRSanchezflavio <flaviosanchez213@gmail.com>
2024-12-11 16:11:36 -0300

    Creación del Back End

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import{BrowserRouter, Routers} from '
 import Navbar from './Components/layouts/navbar/Navbar';
-import Login from '../src/pages/login/Login';
+import Login from './pages/login/Login';
 import Footer from './Components/layouts/footer/Footer';
-import Main from '../src/pages/Home/HomeContainer';
+import Main from './pages/Home/HomeContainer';
 
 const App = () => {
   return (
