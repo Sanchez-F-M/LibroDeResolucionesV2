c2560ce5295205f4d36e630941ec7b21ef42aa69
FRSanchezflavio <flaviosanchez213@gmail.com>
2025-01-10 14:59:27 -0300

    guardado 10/01 v2

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,22 +3,24 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/layouts/navbar/Navbar';
 import Login from './pages/login/Login';
 import Footer from './Components/layouts/footer/Footer';
-import Main from './pages/Home/HomeContainer';
+import HomeContainer from './pages/Home/HomeContainer';
+import Cargas from './pages/Cargas/Cargas';
+import Busquedas from './pages/busquedas/busquedas';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path={'/'} element={<Login />} />
-        <Route path={'/home'} element={<HomeContainer />} />
-        <Route path={'/buscador'} element={<Busquedas />} />
-        <Route path={'/cargas'} element={<Cargas />} />
-        {/* <Route path={'*'} element={} /> */}
-      </Routes>
-      <Footer />
-    </div>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path={'/'} element={<Login />} />
+          <Route path={'/home'} element={<HomeContainer />} />
+          <Route path={'/buscador'} element={<Busquedas />} />
+          <Route path={'/cargas'} element={<Cargas />} />
+          {/* <Route path={'*'} element={} /> */}
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 };
