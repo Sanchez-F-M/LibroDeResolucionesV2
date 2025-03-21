287a2e91847eed314ccfe68c5bc13dc05e158e97
Matias Sanchez <Matias.Sanchez0724@jala.university>
2025-03-20 21:13:57 -0300

    cambios backend

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use('/api', routes)
 
