1cf94eead0b452d7d0c2ad4238a906e5c0e735b6
Flavio Sanchez <flaviosanchez213@gmail.com>
2025-05-11 09:53:00 -0300

    preparando deploy del front

--- front/src/api/api.js
+++ front/src/api/api.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+const maxUploadSize = import.meta.env.VITE_UPLOAD_MAX_SIZE;
+
 const api = axios.create({
-  baseURL: 'http://localhost:3000', 
+  baseURL: apiUrl,
+  timeout: 10000, // 10 segundos
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 api.interceptors.request.use(config => {
